160c196db2bf6746a6f30b6220d0afbce041126f
moody-jnan <jithya.n@outlook.com>
2019-08-12 23:40:53 +1000

    Finished processing 4 fields in total in sample 	- grant_id 	- patent_title 	- abstract 	- number_of_claims

--- data-wrangling-asg1/sample.py
+++ data-wrangling-asg1/sample.py
@@ -6,9 +6,17 @@ class Patent:
    __us_patent_grant_tag_p = re.compile("<us-patent-grant.*>")
    __docnum_filename_p = re.compile('file="[A-Z0-9]*')
    __file_and_dash_p = re.compile('file="')
+
    __invention_title_tag_p = re.compile('<invention-title\sid=".*">.*<\/invention-title>')
    __only_invention_title_tag_p = re.compile('(<invention-title\sid=".*">|<\/invention-title>)')
+
    __abstract_tag_p = re.compile('<abstract id="abstract">[\s\S]*<\/abstract>')
+   __non_abstract_text = re.compile('(<abstract id="abstract">|<\/abstract>|<p.*">|<\/p>)')
+   __remove_tags_p = re.compile('(<[\s\w=\-"]*>|<\/[-\w]+>)')
+   __num_claims_p = re.compile('<number-of-claims>\d*<\/number-of-claims>')
+
+   __codes = [{"find": re.compile("&#x2018;"), "replace":"\u2018"},
+   {"find": re.compile("&#x2019;"), "replace":"\u2019"}]
 
    def __init__(self, raw_xml):
       self.raw = raw_xml
@@ -39,18 +47,27 @@ class Patent:
       debug(invention_title_tag, Patent.__invention_title_tag_p, enable_log)
 
       title = re.sub(Patent.__only_invention_title_tag_p, "", invention_title_tag[0])
-      title_1 = re.sub("(&#x2018;)", "\u2018", title)
-      return re.sub("(&#x2019;)", "\u2019", title_1)
+      return self.cleanup(title)
+
 
    def extract_abstract(self):
+      enable_log = True
+      abstract_tag = re.findall(Patent.__abstract_tag_p, self.raw)
 
-      return "None"
+      if len(abstract_tag) == 0:
+         return "NA"
+      else:
+         debug(abstract_tag, Patent.__abstract_tag_p, enable_log)
+         abstract = re.sub(Patent.__remove_tags_p, "", abstract_tag[0])         
+         return self.cleanup(abstract)
 
    def extract_kind(self):
       return "None"
 
    def extract_number_of_claims(self):
-      return "None"
+      num_claims_tag = re.findall(Patent.__num_claims_p, self.raw)
+      num_claims = re.sub(Patent.__remove_tags_p, "", num_claims_tag[0])
+      return int(self.cleanup(num_claims))
 
    def extract_inventors(self):
       return "None"
@@ -64,6 +81,12 @@ class Patent:
    def extract_claims_text(self):
       return "None"
 
+   def cleanup(self, string):
+      i = 0
+      while i < len(Patent.__codes):
+         string = re.sub(Patent.__codes[i]["find"], Patent.__codes[i]["replace"], string)
+         i += 1
+      return string.rstrip().lstrip()
 
 def main(sample):
    xmlpattern = re.compile(r"<us-patent-grant[\s\S]*?</us-patent-grant>")
@@ -91,6 +114,7 @@ def test_field(patents, sample, field_name):
          if sample_field == patent_field:
             num_correct += 1
          else:
+            #comp(patent_field, sample_field)
             print("Field %s :(%s) does not match one in sample (%s) [%s]" %(field_name, patent_field, sample_field, patent.grant_id))
    
    if num_grants == len(patents) and num_correct == num_grants:
@@ -98,6 +122,22 @@ def test_field(patents, sample, field_name):
    else:
       print("FAILED '%s' test with %d failures" %(field_name, num_grants - num_correct))
 
+def comp(str1, str2):
+   lens1 = len(str1)
+   lens2 = len(str2)
+
+   if lens1 != lens2:
+      print("Both strings are not an equal length")
+   
+   i = 0
+   while i < lens1:
+      
+      if str1[i] != str2[i]:
+         print("First non-match: [%s] to [%s]" %(str1[i], str2[i]))
+         return
+      i+=1
+   
+   print("Both strings are equal")
 
 def test_grant_id(patents, sample):   
    num_grants = len(sample.keys())
@@ -127,12 +167,13 @@ if __name__ == "__main__":
    sample_input = xml_file.read()
    xml_file.close()
 
-   json_file =  open('D:\\Workspace\\postgraduate-projects\\data-wrangling-asg1\\sample_output.json')
+   json_file =  open('D:\\Workspace\\postgraduate-projects\\data-wrangling-asg1\\sample_output.json', encoding='utf-8')
    sample_output = json.load(json_file)
    json_file.close()
 
    patents = main(sample_input)
    test_grant_id(patents, sample_output)
    test_field(patents, sample_output, 'patent_title')
-   #test_field(patents, sample_output, 'abstract')
+   test_field(patents, sample_output, 'abstract')
+   test_field(patents, sample_output, 'number_of_claims')
 
